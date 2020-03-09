7a1f38acfb38ec937dfa431de2809acee4e6ebce
unknown <sraviofficial.13@gmail.com>
2020-03-09 23:28:58 +0530

    first commit

---
+++ resources/views/emails/welcome-email.blade.php
@@ -0,0 +1,16 @@
+@component('mail::message')
+# Welcome to Ravi
+
+The body of your message.
+
+This is a community of fellow developers and we love that you have joined us.
+
+<!-- @component('mail::button', ['url' => ''])
+Button Text
+@endcomponent -->
+
+Thanks,<br>
+Ravi Singh
+
+<!-- {{ config('app.name') }} -->
+@endcomponent
